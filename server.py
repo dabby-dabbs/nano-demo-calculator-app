15ba097edb2c24cf7d4c2343f6568ef6dec33e54
dabby-dabbs <dabbaraharsha@gmail.com>
2023-08-15 22:40:53 +0530

    updaye

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello World!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    dic = request.get_json()
+    return jsonify({'result': dic['first'] + dic['second']})
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    dic = request.get_json()
+    return jsonify({'result': dic['first'] - dic['second']})
+    
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
